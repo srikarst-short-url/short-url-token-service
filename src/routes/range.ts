29cf65d062ccbf54b460b187d3d0c15677ce5d4d
srikarst <srikarsuri@gmail.com>
2023-09-20 17:14:20 +0000

    Change window size of range

--- src/routes/range.ts
+++ src/routes/range.ts
@@ -7,16 +7,15 @@ router.get("/api/range", async (req: Request, res: Response) => {
   let existing = await Range.findOne();
   if (!existing) {
     let newRange = {
-      range: [0, 100]
-    }
+      range: [0, 10],
+    };
     let rangeDoc = Range.build(newRange);
     await rangeDoc.save();
-    existing = await Range.findOne()
-  }
-  else {
-    existing.set({ range: [existing.range[1] + 1, existing.range[1] + 100] });
+    existing = await Range.findOne();
+  } else {
+    existing.set({ range: [existing.range[1] + 1, existing.range[1] + 10] });
     await existing.save();
-    existing = await Range.findOne()
+    existing = await Range.findOne();
   }
 
   res.status(200).send(existing);
